aeb84cc7269acc28e5f20c6ad3f4e81d69a2b340
Taras Kosyi <taras.kosyy@gmail.com>
2021-07-16 20:50:33 +0300

    added GET handler for the server

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,35 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const { createReadStream } = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
+  const isNestedPath = pathname.includes('/');
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      if (isNestedPath) {
+        res.statusCode = 400;
+        res.end('bad request');
+      }
 
+      const rs = createReadStream(filepath);
+      rs.pipe(res);
+
+      rs.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('file not found');
+        } else {
+          res.statusCode = 500;
+          res.end('something went wrong');
+        }
+      });
+      
       break;
 
     default:
