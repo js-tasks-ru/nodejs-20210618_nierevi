0a568495fca3c75b2b89d8c388028f040c458030
Taras Kosyi <taras.kosyy@gmail.com>
2021-07-20 10:58:12 +0300

    added delete request handler

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,16 +1,37 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
+const setResponse = (res, code, message) => {
+  res.statusCode = code;
+  res.end(message);
+}
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
+  const isNestedPath = pathname.includes('/');
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (isNestedPath) {
+        setResponse(res, 400, `bad request ${pathname}`);
+        break;
+      }
+      
+      req.on('error', (err) => {
+        setResponse(res, 500, `unknown error: ${err.message}`);
+      });
+
+      if (fs.existsSync(filepath)) {
+        fs.unlinkSync(filepath);
+        setResponse(res, 200, `file '${pathname}' has been deleted`);
+      } else {
+        setResponse(res, 404, `there is no file '${pathname}'`);
+      }
 
       break;
 
