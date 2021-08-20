6d7088f8f2d3a4f85c38edf6795ff6edc15137d5
Taras Kosyi <taras.kosyy@gmail.com>
2021-08-20 14:32:44 +0300

    feat(07-03): +products search by query

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,26 @@
+const Product = require('../models/Product');
+
+const productDTO = function (doc) {
+  return {
+    id: doc._id,
+    title: doc.title,
+    description: doc.description,
+    price: doc.price,
+    category: doc.category,
+    subcategory: doc.subcategory,
+    images: doc.images,
+  }
+};
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const keywords = ctx.query.query;
+
+  const products = await Product.find(
+    { $text: { $search: keywords } },
+    { score: { $meta: "textScore"} },
+  ).sort( { score: { $meta: "textScore"} } );
+
+  ctx.body = {
+    products: products.map(productDTO),
+  }
 };
